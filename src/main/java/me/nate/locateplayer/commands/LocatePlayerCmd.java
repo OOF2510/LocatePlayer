d0dae9e5c7bd01fbb9bfa8afd5dbae10e43bf42f
OOF2510 <gamekid01234@gmail.com>
2021-02-06 18:08:51 -0500

    shows dimension that player is in now

--- src/main/java/me/nate/locateplayer/commands/LocatePlayerCmd.java
+++ src/main/java/me/nate/locateplayer/commands/LocatePlayerCmd.java
@@ -2,11 +2,14 @@ package me.nate.locateplayer.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+
+import static org.bukkit.World.Environment.*;
 
 public class LocatePlayerCmd implements CommandExecutor {
     @Override
@@ -16,16 +19,35 @@ public class LocatePlayerCmd implements CommandExecutor {
             Player p = Bukkit.getServer().getPlayer(ps);
             String pn = p.getName();
             Location pl = p.getLocation();
+            World.Environment di = pl.getWorld().getEnvironment();
             String plsy = String.valueOf(Math.round(pl.getY()));
             String plsx = String.valueOf(Math.round(pl.getX()));
             String plsz = String.valueOf(Math.round(pl.getZ()));
             String pls = plsy+", "+plsx+", "+plsz;
             if (sender instanceof Player) {
                 Player s = (Player) sender;
-                s.sendMessage(pn+" is at "+pls);
+                if (di == NORMAL) {
+                    String dim = "The Overworld";
+                    s.sendMessage(pn+" is at "+pls+" in "+dim);
+                } else if (di == NETHER) {
+                    String dim = "The Nether";
+                    s.sendMessage(pn+" is at "+pls+" in "+dim);
+                } else if (di == THE_END) {
+                    String dim = "The End";
+                    s.sendMessage(pn+" is at "+pls+" in "+dim);
+                }
             } else {
                 ConsoleCommandSender c = Bukkit.getServer().getConsoleSender();
-                c.sendMessage(pn+" is at "+pls);
+                if (di == NORMAL) {
+                    String dim = "The Overworld";
+                    c.sendMessage(pn+" is at "+pls+" in "+dim);
+                } else if (di == NETHER) {
+                    String dim = "The Nether";
+                    c.sendMessage(pn+" is at "+pls+" in "+dim);
+                } else if (di == THE_END) {
+                    String dim = "The End";
+                    c.sendMessage(pn+" is at "+pls+" in "+dim);
+                }
             }
         }
 
